1a0c5eedc6ddaa8d36c1641d7064420121ce2569
manjusaka <702834491@qq.com>
2016-05-18 17:36:53 +0800

    Default

--- Admin/Conf/config.php
+++ Admin/Conf/config.php
@@ -3,5 +3,4 @@ return array(
 	//'配置项'=>'配置值'
   'DEFAULT_CONTROLLER'    =>  'Index', // 默认控制器名称
   'DEFAULT_ACTION'        =>  'index', // 默认操作名称
-  'URL_MODEL'             =>  2
 );
--- Admin/Controller/IndexController.class.php
+++ Admin/Controller/IndexController.class.php
@@ -2,18 +2,10 @@
 namespace Admin\Controller;
 use Think\Controller;
 class IndexController extends Controller {
-  function _initialize(){
-    if(session('login')!=1){
-      $this->login();
-    }
-    return false;
-  }
+
   public function index(){
     $this->display();
   }
 
-  //登录
-  function login(){
-    $this->display();
-  }
+
 }
